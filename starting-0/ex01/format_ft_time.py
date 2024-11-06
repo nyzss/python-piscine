2158061fdf323e6eaebec96daa59083c4579e00a
Okan Koca <oknakoca@gmail.com>
2024-11-06 16:26:56 +0100

    ex01 time convertion and scientific notation

--- starting-0/ex01/format_ft_time.py
+++ starting-0/ex01/format_ft_time.py
@@ -1,3 +1,8 @@
 from datetime import datetime
 
-print(datetime.now())
+timestamp = datetime.now().timestamp()
+
+epoch = datetime.fromtimestamp(0).strftime("%B %-d, %Y")
+
+print(f"Seconds since {epoch}: {timestamp:,} or {timestamp:.2e} in scientific notation")
+print(datetime.now().strftime("%b %d %Y"))
